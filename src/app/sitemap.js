e2f8c536820b47667c554b1b819fd2a9762f937b
TruongVu049 <nguyenvu492003@gmail.com>
2024-08-18 18:08:04 +0700

    update file

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,14 +3,15 @@ const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : "http://localhost:3000";
 
-export default function sitemap() {
-  const blogRoutes = getPosts().then((post) =>
+export const dynamic = "force-dynamic";
+
+export default async function sitemap() {
+  const blogRoutes = await getPosts().then((post) =>
     post.map((item) => ({
       url: `${baseUrl}/bai-viet/${item.slug}`,
       lastModified: item.updatedAt,
     }))
   );
-
   const routes = ["", "dang-nhap", "dang-ky", "tim-kiem"].map((route) => ({
     url: `${baseUrl}/${route}`,
     lastModified: new Date().toISOString(),
